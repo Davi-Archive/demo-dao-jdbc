fc7b0acc5b4650039db8d8307d8de292c8315cb0
d1av <davi4alves@gmail.com>
2022-11-27 00:02:17 -0300

    feat: add delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner scanner = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println("=== TEST 1: seller findById =====");
@@ -38,13 +40,21 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
-		
+
 		System.out.println();
 		System.out.println("=== TEST 5: seller update =====");
 		seller = sellerDao.findById(7);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed.");
+
+		System.out.println();
+		System.out.println("=== TEST 6: seller delete =====");
+		int id = scanner.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+
+		scanner.close();
 	}
 
 }
